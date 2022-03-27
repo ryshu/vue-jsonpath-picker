9165579aceaa0f2e0ebc1aaef20758dae7e0f8db
Oscar MARIE--TAILLEFER <oscar.marie-taillefer@spikeelabs.fr>
2022-03-27 22:52:16 +0200

    fix: new build using new packages availables

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import JSONPathPicker from "./JSONPathPicker.vue";
 
 import 'jsonpath-picker-vanilla/lib/jsonpath-picker.min.css';
 
-export default {
- install(Vue) {
-  // Let's register our component globally
-  // https://vuejs.org/v2/guide/components-registration.html
-  Vue.component("jsonpath-picker", JSONPathPicker);
- }
-};
+export const config = {
+    install(Vue) {
+        // Let's register our component globally
+        // https://vuejs.org/v2/guide/components-registration.html
+        Vue.component("jsonpath-picker", JSONPathPicker);
+    }
+}
+
+export default config;
